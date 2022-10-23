e32e93770995303214e7e98abb891b5c59b3a576
Anastasiya Nekrasova <afierycat@gmail.com>
2022-10-23 17:08:01 +0300

    #block2

---
+++ lr5/maven-demo/src/test/java/com/bstu/nekrasova/AppTest.java
@@ -0,0 +1,20 @@
+package com.bstu.nekrasova;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
